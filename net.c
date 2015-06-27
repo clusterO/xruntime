50dbf62abec78c6c88ac078c3be94df11b867555
cluserO <fourmou.m@gmail.com>
2015-06-27 12:23:07 +0100

    Implementing Network lib - Gets inAddr

---
+++ net.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include "net.h"
+
+#define BACKLOG 10
+
+void *getInAddr(struct sockaddr *sa) 
+{
+    if(sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int getSocketListner(char *port) 
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int yes = 1;
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return -1;    
+    }
+
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        
+    }
+}
