e29390e338fae42a95d51d47afc62b031f851898
cluserO <fourmou.m@gmail.com>
2015-07-09 14:31:25 +0100

    Implementing linked list

--- llist.h
+++ llist.h
@@ -6,17 +6,17 @@ struct llist {
 	int count;
 };
 
-extern struct llist *createLList(void);
-extern void destroy(struct llist *llist);
-extern void *insert(struct llist *llist, void *data);
-extern void *append(struct llist *llist, void *data);
-extern void *head(struct llist *llist);
-extern void *tail(struct llist *llist);
-extern void *find(struct llist *llist, void *data, int (*cmpfn)(void *, void *));
-extern void *deleteL(struct llist *llist, void *data, int (*cmpfn)(void *, void *));
-extern int count(struct llist *llist);
-extern void forEach(struct llist *llist, void (*f)(void *, void *), void *arg);
-extern void **getArray(struct llist *llist);
-extern void freeArray(void **a);
+extern struct llist *llcreate(void);
+extern void lldestroy(struct llist *llist);
+extern void *llinsert(struct llist *llist, void *data);
+extern void *llappend(struct llist *llist, void *data);
+extern void *llhead(struct llist *llist);
+extern void *lltail(struct llist *llist);
+extern void *llfind(struct llist *llist, void *data, int (*cmpfn)(void *, void *));
+extern void *lldelete(struct llist *llist, void *data, int (*cmpfn)(void *, void *));
+extern int llcount(struct llist *llist);
+extern void llforEach(struct llist *llist, void (*f)(void *, void *), void *arg);
+extern void **llgetArray(struct llist *llist);
+extern void llfreeArray(void **a);
 
 #endif
